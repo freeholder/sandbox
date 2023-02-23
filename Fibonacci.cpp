f05b4c084030170124ad8541a3f1ee7672ed96db
freeholder <126153617+freeholder@users.noreply.github.com>
2023-02-23 12:35:11 +0300

    Update Fibonacci.cpp

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 15; // Номер числа Фибоначчи
+    int *ar = new int[n + 1];
+    ar[0] = 0;
+    ar[1] = 1;
+    for (int i = 2; i <= n; i++) 
+    {
+        ar[i] += ar[i - 1] + ar[i - 2];
+    }
+
+    cout << ar[n - 1];
+}
 
